f51b756d606c10662eb2e18da60def1c5888ab63
Mati <mswiatek2002@gmail.com>
2023-04-30 16:14:47 +0200

    proby v2

--- server_remote_file/main.cpp
+++ server_remote_file/main.cpp
@@ -3,6 +3,7 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <map> // słowniki
+#include <vector>
 
 /* może jsona się jeszcze dowali do tego
 #include <nlohmann/json.hpp>
@@ -18,22 +19,39 @@ using namespace std;
 
 class User{
 public:
-    User(string login, string hash_pass, int acc_type = 1);
-    bool login_try(string login,string hash_pass);
+    User() : _login(0){}
+    User(string login, string hash_pass, int acc_type = 0) : _login(login), _hashpassword(hash_pass), _is_logged(0), _count_login_try(0), _block(0), _account_type(acc_type){}
+    int try_login(string login, string password){
+        if(login==_login){
+            if(password==_hashpassword){
+                _is_logged = 1;
+                return 1;
+            }
+            _count_login_try++;
+            return -1;
+        }
+        return 0;
+    }
+    string getlogin(){return _login;}
+    bool get_is_logged(){return _is_logged;}
 
 private:
     string _login;
     string _hashpassword;
     int _count_login_try;
-    int _account_type;
-
+    int _account_type; // 1 admin, 0 user
+    bool _is_logged;
+    bool _block; // czy jest zablokowany
 };
 
-string przychodzace(SOCKET socket){
+vector<User> users;
+
+
+string przychodzace(SOCKET socket){ // tu dorobić sprawdzanie czy to co przychodzi jest stringiem
     int recv_bits;
     char recv_frame[1024]; // musimy gdzieś zapisywać to co przychodzi
     recv_bits = recv( socket, recv_frame, 1024, 0 );
-    if( recv_bits == 0 || recv_bits == WSAECONNRESET ) { strcpy(recv_frame,"Connection closed / Error" ) ; } // jeśli nie otrzymaliśmy odpowiedzi
+    if( recv_bits == 0 || recv_bits == WSAECONNRESET ) { strcpy(recv_frame,"-1" ) ; } // jeśli nie otrzymaliśmy odpowiedzi
     return recv_frame;
 }
 
@@ -44,23 +62,36 @@ int wychodzace(SOCKET socket, string message){
 }
 
 int main() {
+    // dodawanie admina
+    User admin("admin", "admin", 1);
+    users.push_back(admin);
+
+    // dorobić czy konfiguracja czy normalna praca.
+    // konfugiruacja jako oddzielna funkcja, aby admin tez mogl wejsc / ryzykowne
+
+
+    // to wszystko do funkcji walnąć
     string IP_serv = "192.168.56.1";
     int Port = 9021;
 
-    SOCKET gniazdo_root, gniazdo1;
+    SOCKET gniazdo_root, client_socket;
     struct sockaddr_in server, client; // instancje struktury które przechowują dane
 
     WSADATA wsaData;
-    int result = WSAStartup( MAKEWORD(2,2), &wsaData); // używamy wersji 2.2
-    if(result != NO_ERROR) cout << "Initialization error.\n"; // jeśli się nie udało to dostajemy komunikat
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != NO_ERROR) {
+        cerr << "Initialization error.\n"; // jeśli się nie udało to dostajemy komunikat
+        return 1; // wystąpił błąd
+    }
 
     gniazdo_root = socket( AF_INET, SOCK_STREAM, 0 ); // IPv4, IPPROTO_TCP
     if( gniazdo_root == SOCKET_ERROR ){ // jeśli jest jakiś błąd
-        cout << "Initialization error.\n Error creating socket: %d\n" << WSAGetLastError() << endl; // wypisze nam ostatni blad
+        cerr << "Initialization error.\n Error creating socket: %d\n" << WSAGetLastError() << endl; // wypisze nam ostatni blad
         WSACleanup(); // "sprzątanie" po WSA
-        return 0;
+        return 1;
     }
 
+    // podpięcie gniazda pod port
     memset( & server, 0, sizeof( server ) ); // pamięć na 0, są domyślne wartości w polach struktury
     server.sin_family = AF_INET; // używamy protokołu IPv4
     server.sin_addr.s_addr = inet_addr( "192.168.56.1" );
@@ -68,69 +99,92 @@ int main() {
 
     // ================ program dla Servera =================================
 
-    if( bind( gniazdo_root,( sockaddr * ) &server, sizeof( server ) ) == SOCKET_ERROR )  // przypisanie adresu(gniazdo ma ten adres) , https://man7.org/linux/man-pages/man2/bind.2.html
+    if(bind( gniazdo_root,( sockaddr* ) &server, sizeof( server ) ) == SOCKET_ERROR )  // przypisanie adresu(gniazdo ma ten adres) , https://man7.org/linux/man-pages/man2/bind.2.html
     {
-        cout << "bind() failed." << WSAGetLastError() << endl;
+        cerr << "bind() failed." << WSAGetLastError() << endl;
         closesocket( gniazdo_root );
-        return 0;
+        return 1;
     }
 
-    if( listen( gniazdo_root, 1 ) == SOCKET_ERROR ) // max połączeń oczekujących  https://man7.org/linux/man-pages/man2/listen.2.html
-        cout << "Error listening" << endl ;
+    if( listen( gniazdo_root, 1 ) == SOCKET_ERROR ) { // max połączeń oczekujących  https://man7.org/linux/man-pages/man2/listen.2.html
+        cerr << "Error listening" << endl;
+        return 1;
+    }
 
-
-/*
- *      Miejsce na różne dane do działania i wgl na zmienne etc
- */
-
+// wszystko powyzej można do funkcji ?
 
     while(1){ // postawienie servera, teraz już cały czas działa i czeka na połączenie
-        string message, login, mes_to_send, hash_password;
-
         int dlugosc=sizeof(client); // !!!! ważne fest XD
 
         cout << "Oczekiwanie na polaczenie..." << endl;
-        gniazdo1 = accept(gniazdo_root, (struct sockaddr *) &client, &dlugosc); // gniazdo1 teraz odpowiada za połączenie
-        if (gniazdo1 == -1) { WSACleanup(); cout << "blad accept\n" << endl; return 0;}
+        client_socket = accept(gniazdo_root, (struct sockaddr *) &client, &dlugosc); // client_socket teraz odpowiada za połączenie
+        if (client_socket == INVALID_SOCKET) {closesocket(gniazdo_root); cerr << "blad accept\n" << endl; return 1;}
 
-        else cout <<"zaakceptowano\n" << endl;
-        while(1){ //obsługa połączenia przychodzącego, nasz socket to gniazdo1
+        // sprawdzanie adresu IP przychodzącego, można byłoby jakoś przed nawiązaniem połączenia to zrobić
+        /*
+        char clientIp[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &(client.sin_addr), clientIp, INET_ADDRSTRLEN);
+        cout << "Połączenie nawiązane z klientem o adresie IP: " << clientIp << endl;
+        */
+
+        while(1){ //obsługa połączenia przychodzącego, nasz socket to client_socket
+            string message, login, hash_password;
             int status=0;
-            while(!status) {
-                message = "Witaj podaj login";
-                status = wychodzace(gniazdo1, message);
-            }
 
-            login = przychodzace(gniazdo1);
+            // moze to tez moznaby było dać do funkcji?
+            message = "Witaj podaj login";
+            status = wychodzace(client_socket, message);
+            if(!status){break;}
+
+            login = przychodzace(client_socket);
+            if( login == "-1"){cerr << "error przy odbieraniu danych"; break;}
             cout << "login to #" << login << "#" << endl;
 
-            status=0;
-            while(!status) {
             message = "podaj haslo";
-            status = wychodzace(gniazdo1, message);
-            }
-            hash_password = przychodzace(gniazdo1);
+            status = wychodzace(client_socket, message);
+            if(!status){break;}
+
+            hash_password = przychodzace(client_socket);
+            if( login == "-1"){cerr << "error przy odbieraniu danych"; break;}
             cout << "hash_password to #" << hash_password << "#" << endl;
 
-            auto para = users.find(login); // od C++11  https://cpp0x.pl/kursy/Kurs-C++/Poziom-5/Kontenery-asocjacyjne-std-set-i-std-map/589
+            User zalogowany_user;
+            bool zalogowano=0;
+            for (auto& user : users) {
+                int stat;
+                if(user.getlogin() == login) { // aby przy wiekszej ilosci przyspieszyc
+                    stat = user.try_login(login, hash_password);
+                    status = wychodzace(client_socket, to_string(stat)); // wysylamy stan
+                    przychodzace(client_socket); // cokolwiek
+                    if(stat == 1) {
+                        zalogowany_user=user;
+                        zalogowano=1;
+                    }
+                    break;
+                }
+            }
+            if(zalogowano==0){ continue;}
+
+            // gdy zalogowano:
+
 
             if ( para == users.end() ) { // 1 - nie ma loginu, 2- bledne haslo, 3 - prezszlo
-                status = wychodzace(gniazdo1, "1");
+                status = wychodzace(client_socket, "1");
                 if(!status) { cout << "send error"; break; }
-                message = przychodzace(gniazdo1);
+                message = przychodzace(client_socket);
                 cout << message[0];
                 if( message[0] == '0') break;
                 else continue;
             }
             else if(para->second != hash_password) { // jeśli hash haseł jest różny 2
                     // mes_to_send = "2"+to_string(count_try);
-                    status = wychodzace(gniazdo1, "2"+to_string(count_try));
+                    status = wychodzace(client_socket, "2" + to_string(count_try));
                     count_try--; // zmniejszamy ilość prob
                     if(!status) { cout << "send error"; break; }
-                    message = przychodzace(gniazdo1);
+                    message = przychodzace(client_socket);
                     cout << message[0];
                     if(count_try == -1){
-                        status = wychodzace(gniazdo1, "nie mozesz juz probowac. skontaktuj sie z adminem");
+                        status = wychodzace(client_socket, "nie mozesz juz probowac. skontaktuj sie z adminem");
                         if(!status) { cout << "send error"; break; }
                         hack = 1;
                         break;
@@ -147,13 +201,13 @@ int main() {
                                             "   sorki, no tak, informatycy nie posiadaja kobiet\n"
                                             "4. To napisz algorytm na szukanie kobiety, moze pomoze\n""";
                     mes_to_send = "3" + dostepne_funkcje;
-                    status = wychodzace(gniazdo1, mes_to_send);
+                    status = wychodzace(client_socket, mes_to_send);
                     cout << "wyslalismy cos" << mes_to_send;
 
                     if(!status) { cout << "send error"; break; }
 
                     cout << "normalny program" << endl;
-                    przychodzace(gniazdo1);
+                    przychodzace(client_socket);
                     break;
                 }
             }
@@ -163,7 +217,7 @@ int main() {
              */
             break;
         }
-        closesocket(gniazdo1);
+        closesocket(client_socket);
     }
 
 
